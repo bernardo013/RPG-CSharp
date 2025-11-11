0f0419f54b4aaab2cee20b4554a2d4e730dbb491
Bernardo Marques <japatvs1@gmail.com>
2025-11-11 11:04:34 -0300

    Adicionar arquivos de projeto.

---
+++ RPG CSharp/Personagens/Guerreiro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_CSharp.Personagens
+{
+    internal class Guerreiro : Personagem
+    {
+        public Guerreiro(string Nome, int Vida, int Ataque, int Defesa) 
+            : base(Nome, Vida, Ataque, Defesa) { }
+
+        public override void Atacar(Personagem alvo)
+        {
+            base.Atacar(alvo);
+        }
+
+        public override void ReceberDano(int danoRecebido)
+        {
+            base.ReceberDano(danoRecebido);
+        }
+
+    }
+}
---
+++ RPG CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using RPG_CSharp.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Batalha();
+        }
+
+        public static void Batalha() {
+            Mago Gandalf = new Mago("Gandalf", 100, 30, 5);
+            Guerreiro Euclides = new Guerreiro("Euclides", 150, 10, 15);
+            Gandalf.Atacar(Euclides);
+            Euclides.ExibirStatus();
+
+        
+        }
+    }
+}
